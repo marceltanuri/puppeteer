91601b9fa1d154b34c35895632fbdbca72f0c1b4
Marcel Tanuri (Inetum) <marcel.tanuri@inetum.com>
2022-03-31 00:08:21 +0100

    remove unused echo

--- open.ps1
+++ open.ps1
@@ -3,6 +3,5 @@ node $PSScriptRoot/index.js
 $downloadsDir="C:\Users\marce\Downloads"
 
 $latestDownload=Get-ChildItem $downloadsDir *.ica -Name | Sort-Object -Descending -Property LastWriteTime | select -First 1
-$latestDownload
 $latestDownload=$downloadsDir+"\"+$latestDownload
 & $latestDownload
